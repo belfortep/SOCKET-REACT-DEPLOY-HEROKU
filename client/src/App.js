4e3312010f752c75bb1d5d856c0ba7901a28f3e5
belfortep <pbelforte2003@gmail.com>
2022-08-04 08:25:05 -0300

    added css and preparing for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -49,19 +49,22 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
 
-      <form onSubmit={handleSubmit}>
-        <input type='text' onChange={e => setMessage(e.target.value)} value={message} />
-        <button>Send</button>
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className='text-2xl font-bold my-2'>Chat</h1>
+        <input type='text' onChange={e => setMessage(e.target.value)} value={message} className="border-2 border-zinc-500 p-2 text-black w-full" />
+        <ul className='h-80 overflow-y-auto'>
+          {messages.map((message, index) => (
+            <li key={index} className={`my-2 p-2 table text-sm rounded-md ${message.from === "Yo" ? "bg-sky-700 ml-auto" : "bg-blakc"}`}>
+              <p>{message.from}: {message.body}</p>
+            </li>
+          ))}
+        </ul>
 
       </form>
 
-      {messages.map((message, index) => (
-        <div key={index}>
-          <p>{message.from}: {message.body}</p>
-        </div>
-      ))}
+
 
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ import { Server as SocketServer } from "socket.io";
 import http from 'http';
 import cors from 'cors';
 import { PORT } from './config.js';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
-        origin: '*'
+        origin: 'http://localhost:3000'
     }
 });
 
@@ -29,6 +32,8 @@ io.on('connection', (socket) => {
         });
     });
 });
+
+app.use(express.static(join(__dirname, '../client/build')))
 
 server.listen(PORT);
 
